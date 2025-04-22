74baca1729436564f7d0792fbe4b750856b240d2
Shevch376 <symaexplay5430@gmail.com>
2025-04-22 22:39:24 +0700

    login/logout

--- core/Src/Application.php
+++ core/Src/Application.php
@@ -6,13 +6,32 @@ use Error;
 use Illuminate\Container\Container;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Database\Capsule\Manager as Capsule;
-
+use Src\Auth\Auth;
 
 class Application
 {
     private Settings $settings;
     private Route $route;
     private Capsule $dbManager;
+    private Auth $auth;
+
+    public function __construct(Settings $settings)
+    {
+        //Привязываем класс со всеми настройками приложения
+        $this->settings = $settings;
+        //Привязываем класс маршрутизации с установкой префикса
+        $this->route = Route::single()->setPrefix($this->settings->getRootPath());
+        //Создаем класс менеджера для базы данных
+        $this->dbManager = new Capsule();
+        //Создаем класс для аутентификации на основе настроек приложения
+        $this->auth = new $this->settings->app['auth'];
+
+        //Настройка для работы с базой данных
+        $this->dbRun();
+        //Инициализация класса пользователя на основе настроек приложения
+        $this->auth::init(new $this->settings->app['identity']);
+    }
+
 
     public function __get($key)
     {
--- core/Src/Route.php
+++ core/Src/Route.php
@@ -8,10 +8,12 @@ use FastRoute\RouteCollector;
 use FastRoute\RouteParser\Std;
 use FastRoute\DataGenerator\MarkBased;
 use FastRoute\Dispatcher\MarkBased as Dispatcher;
-
+use Src\Traits\SingletonTrait;
 
 class Route
 {
+    //Используем методы трейта
+    use SingletonTrait;
 
     //Свойство для хранения текущего маршрута
     private string $currentRoute = '';
